f1ab51cda8f7866ef24b5e1d4c47de492b13ee00
Arlei Rodrigues1 <arlei.freire011@gmail.com>
2022-01-13 10:34:45 -0300

    projeto iniciado com a mensagem ola mundo

---
+++ Projeto teste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ola mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
